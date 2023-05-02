3f452619bcde6ae40518c6373d5bea8883deeb0a
Ben Purinton <ben.purinton@gmail.com>
2023-05-02 15:53:12 -0800

    seventh step

--- app.rb
+++ app.rb
@@ -2,33 +2,6 @@
 # objects and methods
 require 'sinatra'
 
-# `settings` is defined in sinatra we want to also 
-# allow the live preview to reload on changes if we 
-# are in development mode
-if settings.development?
-  require "sinatra/reloader"
-
-  # we would also like a nicer error page in development
-  require 'better_errors'
-  require 'binding_of_caller'
-  use(BetterErrors::Middleware)
-  BetterErrors.application_root = __dir__
-  BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
-end
-
-# we use the `set` method with the :port argument to 
-# set what port our live preview shows when we run 
-# `ruby app.rb`
-set(:port, 3000)
-
-# To open .html.erb files, need to register them
-Tilt.register(Tilt::ERBTemplate, 'html.erb')
-
-# alternative to setting the layout in every erb() call
-# we can set the default layout name to use and sinatra 
-# will look for that file
-set(:erb, :layout => :application_layout)
-
 # we define our first route with sinatra's get method
 get('/') do
   # we can pass plain text
