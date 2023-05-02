59f80e0897ebf1ff1b9ce7752e870a3113301fea
Ben Purinton <ben.purinton@gmail.com>
2023-05-02 15:39:01 -0800

    third step

--- app.rb
+++ app.rb
@@ -7,12 +7,22 @@ require 'sinatra'
 # are in development mode
 if settings.development?
   require "sinatra/reloader"
+
+  # we would also like a nicer error page in development
+  require 'better_errors'
+  require 'binding_of_caller'
+  use(BetterErrors::Middleware)
+  BetterErrors.application_root = __dir__
+  BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 end
 
 # we use the `set` method with the :port argument to 
 # set what port our live preview shows when we run 
 # `ruby app.rb`
 set(:port, 3000)
+
+# To open .html.erb files, need to register them
+Tilt.register(Tilt::ERBTemplate, 'html.erb')
 
 # we define our first route with sinatra's get method
 get('/') do
@@ -33,6 +43,6 @@ get('/rock') do
 
   # but even better, we can make a views folder
   # and render a new type of file from there
-  # we call the file "views/rock.erb"
+  # we call the file "views/rock.html.erb"
   erb(:rock)
 end
