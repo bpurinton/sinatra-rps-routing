7ebe637d42fbd2466177c32e149bee8fe221081e
Ben Purinton <ben.purinton@gmail.com>
2023-05-02 15:35:02 -0800

    second step

--- app.rb
+++ app.rb
@@ -22,3 +22,17 @@ get('/') do
   # we can pass HTML
   "<h1>Hello, world!</h1>"
 end
+
+# we can define another route to our rock page
+get('/rock') do
+  # we can simply redirect somewhere
+  # redirect("https://www.wikipedia.org")
+
+  # or we can render an html page from the root
+  # send_file("rock.html")
+
+  # but even better, we can make a views folder
+  # and render a new type of file from there
+  # we call the file "views/rock.erb"
+  erb(:rock)
+end
